388b871c283ea9e3f5843e51f4387ac424af5721
MasterSDW <bibbiblol2@gmail.com>
2026-02-08 00:41:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Дозволяємо CORS для всіх маршрутів
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True) 
+    app.run(debug=True)
